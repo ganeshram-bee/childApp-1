c6fc2b84b85f50999184d8b04dfb6a034d88b93f
ganeshram-bee <ganesh.ram@krazybee.com>
2020-02-05 15:40:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
+	console.log("window event:");
+  window.e.emit('message', {text: 'Hello from childApp1'});
   return (
+  	<BrowserRouter basename={"/parent"}>
     <div className="App">
-      <header className="App-header">
-        Hello from app2
-      </header>
+        Hello from child app 1
+        <p>
+        <button 
+        	onClick={() => {
+        		console.log("informing parent to change pathName");
+        		window.e.emit('goTo', {pathName: '/childApp2'});
+        	}}
+        >
+          Go to childApp2
+        </button>
+        </p>
     </div>
+    </BrowserRouter>
   );
 }
 
